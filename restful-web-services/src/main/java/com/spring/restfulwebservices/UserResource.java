caff54f4a972673a36dbb7613c0ce48de5430cf2
Filip Kotula <fkotula1121@gmail.com>
2022-06-30 14:17:56 +0200

    v1.4.0 post method to create user

--- restful-web-services/src/main/java/com/spring/restfulwebservices/UserResource.java
+++ restful-web-services/src/main/java/com/spring/restfulwebservices/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import User.User;
@@ -23,5 +25,10 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveOneUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 }
