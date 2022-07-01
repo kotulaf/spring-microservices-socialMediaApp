fd95376f7a2d066f90461bca01d1c257fdfe25e3
Filip Kotula <fkotula1121@gmail.com>
2022-07-01 12:40:30 +0200

    v1.9.0 internationalized barely working

--- restful-web-services/src/main/java/com/spring/restfulwebservices/TestController/AppController.java
+++ restful-web-services/src/main/java/com/spring/restfulwebservices/TestController/AppController.java
@@ -1,11 +1,17 @@
 package com.spring.restfulwebservices.TestController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/textStringBean")
     public testStringBean testString(){
@@ -15,5 +21,10 @@ public class AppController {
     @GetMapping("/pathVariableTest/{text}")
     public testStringBean testPathVariable(@PathVariable String text) {
         return new testStringBean(String.format("%s", text));
+    }
+
+    @GetMapping("/internationalized")
+    public String testStringInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
     }
 }
